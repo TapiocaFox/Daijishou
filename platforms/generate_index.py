abef70d17f9cdac39e6e37ff44ec5d3c2762ede9
BuildTools <magneticchen@gmail.com>
2022-03-13 01:11:52 +0800

    update platforms

--- platforms/generate_index.py
+++ platforms/generate_index.py
@@ -1,13 +1,20 @@
 import os, re, json
 indexFilename = "index.json"
+devicesDir = "./devices"
+regex = re.compile("^(?!(?:\._|\.).*).*\.json$")
+
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 files.sort()
+
+# devices = [f for f in os.listdir(devicesDir) if os.path.isdir(f)]
+devices = [f for f in os.listdir(devicesDir)]
+devices.sort()
+
 index = {
     "baseUri": "https://raw.githubusercontent.com/magneticchen/Daijishou/main/platforms/",
     "platformList": []
 }
 for f in files:
-    regex = re.compile("^(?!(?:\._|\.).*).*\.json$")
     if regex.match(f) and f != indexFilename:
         with open(f) as jsonFile:
             platformSharable = json.load(jsonFile)
@@ -17,6 +24,22 @@ for f in files:
                 "platformName": platformEntityPortable['name'],
                 "platformShortname": platformEntityPortable['shortname']
             })
+
+for d in devices:
+    deviceDir = devicesDir+'/'+d
+    files = [f for f in os.listdir(deviceDir) if os.path.isfile(f)]
+    files.sort()
+    for f in files:
+        if regex.match(f) and f != indexFilename:
+            f = deviceDir+'/'+f
+            with open(f) as jsonFile:
+                platformSharable = json.load(jsonFile)
+                platformEntityPortable = platformSharable['platform']
+                index['platformList'].append({
+                    "filename": f,
+                    "platformName": d+' - '+platformEntityPortable['name'],
+                    "platformShortname": platformEntityPortable['shortname']
+                })
 
 with open(indexFilename, 'w') as outfile:
     json.dump(index, outfile, indent=2, sort_keys=True)
