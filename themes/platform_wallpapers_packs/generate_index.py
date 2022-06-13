87a8b07160fe1cd86f8c138d04acfc3dad32b033
magneticchen <magneticchen@gmail.com>
2022-06-14 07:22:32 +0800

    update platform wallpapers packs

---
+++ themes/platform_wallpapers_packs/generate_index.py
@@ -0,0 +1,40 @@
+import os, re, json
+indexFilename = "index.json"
+
+regex = re.compile("^(?!(?:\._|\.).*).*\.json$")
+
+platformWallpapersPacks = [f for f in os.listdir('.') if os.path.isdir(f)]
+platformWallpapersPacks.sort()
+
+index = {
+    "baseUri": "https://raw.githubusercontent.com/magneticchen/Daijishou/main/themes/platform_wallpapers_packs/",
+    "platformWallpapersPackList": []
+}
+
+
+for d in platformWallpapersPacks:
+    print(d)
+    platformWallpapersPackDir = d
+    files = [f for f in os.listdir(platformWallpapersPackDir) if os.path.isfile(platformWallpapersPackDir+'/'+f)]
+    # files.sort()
+
+    for f in files:
+        if f == indexFilename:
+            f = platformWallpapersPackDir+'/'+f
+            with open(f) as jsonFile:
+                platformWallpapersPackIndex = json.load(jsonFile)
+                platformWallpapersPackName = platformWallpapersPackIndex['name']
+                platformWallpapersPackDescription = platformWallpapersPackIndex['description']
+                platformWallpapersPackAuthors = platformWallpapersPackIndex['authors']
+                platformWallpapersPackPreviewThumbnailFilename = platformWallpapersPackIndex['previewThumbnailFilename']
+                index['platformWallpapersPackList'].append({
+                    "platformWallpapersPackRootPath": platformWallpapersPackDir,
+                    # "platformWallpaperPackIndexPath": f,
+                    "platformWallpapersPackPreviewThumbnailPath": platformWallpapersPackDir+'/'+platformWallpapersPackPreviewThumbnailFilename,
+                    "platformWallpapersPackAuthors": platformWallpapersPackAuthors,
+                    "platformWallpapersPackName": platformWallpapersPackName,
+                    "platformWallpapersPackDescription": platformWallpapersPackDescription
+                })
+
+with open(indexFilename, 'w') as outfile:
+    json.dump(index, outfile, indent=2, sort_keys=True)
